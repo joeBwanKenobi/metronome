68677e598db47497a70552ce409e8cb1d5b09fb3
Joe <joe@jo3.io>
2020-03-21 20:52:03 -0400

    sound working

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,8 @@ var triggerHand = document.getElementById('tick-hand');
 var triggerButton = document.getElementById('start');
 var stopButton = document.getElementById('stop');
 var bpmInput = document.getElementById('bpm');
-var audioClip = new Audio('https://www.jo3.io/assets/downloads/metronome.wav');
+var audioClip = document.createElement('audio');
+audioClip.src = 'https://www.jo3.io/assets/downloads/metronome.wav';
 
 class Metronome {
   constructor(elem, bpm, sound) {
@@ -15,29 +16,40 @@ class Metronome {
     this.animation = new gsap.timeline({paused:true, repeat: -1});
     console.log(this.sound);
     this.animation.set(this.elem, {transformOrigin: "bottom center"});
-    this.animation.to(this.elem, {duration: this.tick_dur, rotation: this.rot, ease: this.easeType, onComplete: this.playSound()});
+    this.animation.to(this.elem, {duration: this.tick_dur, rotation: this.rot, ease: this.easeType});
     this.animation.to(this.elem, {duration: this.tick_dur, rotation: 0, ease: this.easeType});
-    this.animation.to(this.elem, {duration: this.tick_dur, rotation: this.rot * -1, ease: this.easeType, onComplete: this.playSound});
+    this.animation.to(this.elem, {duration: this.tick_dur, rotation: this.rot * -1, ease: this.easeType});
     this.animation.to(this.elem, {duration: this.tick_dur, rotation: 0, ease: this.easeType});
   }
+
+  playAnim() {
+    this.animation.play(0);
+    this.timer();
+  }
+  stopAnim() {
+    this.animation.seek(0);
+    this.animation.pause();
+    clearInterval(this.clock);
+  }
+
   playSound() {
-    console.log(this.sound);
-    // this.sound.currentTime = 0;
-    // this.sound.play();
+
   }
-  tick(){
-    console.log('tick')
+  timer(){
+    this.clock = setInterval(() => { this.sound.currentTime = 0; this.sound.play() }, 60000/120);
   }
+
 }
 
-var met = new Metronome('#tick-hand', 120, audioClip);
+// var met = new Metronome('#tick-hand', 120, audioClip);
 
 // bpmInput.addEventListener('input', () => { bpm = bpmInput.value;
 //                                          console.log(bpm);
 //                                          });
-triggerHand.addEventListener('click', () => met.animation.play(0));
-triggerButton.addEventListener('click', () => met.animation.play(0));
+triggerHand.addEventListener('click', () => met.playAnim());
+triggerButton.addEventListener('click', () => met.playAnim());
 stopButton.addEventListener('click', () => {
-  met.animation.seek(0) 
-  met.animation.pause()
+  met.stopAnim();
 });
+
+var met = new Metronome('#tick-hand', 120, audioClip);
