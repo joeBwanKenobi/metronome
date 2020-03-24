8ce50680cc90209c6d60f7ca41db26b5a33890b0
Joe <joe@jo3.io>
2020-03-24 14:44:06 -0400

    Restyled buttons, added sliding weight, still needs number scaling for slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,22 @@
+gsap.registerPlugin(Draggable);
+
 var triggerHand = document.getElementById('tick-hand');
 var triggerButton = document.getElementById('start');
 var stopButton = document.getElementById('stop');
 var bpmInput = document.getElementById('bpm');
+var incr = document.getElementById('increase');
+var decr = document.getElementById('decrease');
 var audioClip = document.createElement('audio');
 audioClip.src = 'https://www.jo3.io/assets/downloads/metronome.wav';
 
 
 class Metronome {
-  constructor(elem, bpm, sound) {
+  constructor(elem, startBpm = 40, sound) {
     this._elem = elem;
-    this._bpm = bpm;
+    this._bpm = startBpm;
+    this._maxBpm = 208;
     // gsap timelines run in seconds convert bpm to bps, each tick has two movements so halve the bps  (center to right, center to left)
-    this._tickDur = (60/bpm) / 2;
+    this._tickDur = (60/startBpm) / 2;
     this._sound = sound;
     this._rot = 8;
     this._easeType = Linear.easeNone;
@@ -46,7 +51,7 @@ class Metronome {
   }
   calcTick() {
     this._tickDur = (60 / this._bpm) / 2;
-    console.log(`new bpm and tickDur: ${this._bpm}, ${this._tickDur}`);
+    // console.log(`new bpm and tickDur: ${this._bpm}, ${this._tickDur}`);
   }
   get bpm() {
     return this._bpm;
@@ -58,7 +63,6 @@ class Metronome {
   }
   // triggered by change in BPM input (focusout event)
   update() {
-    this.calcTick();
     this.calcTick();
     this.stopAnim();
     // create new timeline with updated tick duration
@@ -73,10 +77,37 @@ class Metronome {
 }
 
 bpmInput.addEventListener('focusout', () => met.bpm = bpmInput.value);
+bpmInput.addEventListener('change', () => met.bpm = bpmInput.value);
 triggerHand.addEventListener('click', () => met.playAnim());
 triggerButton.addEventListener('click', () => met.playAnim());
 stopButton.addEventListener('click', () => {
   met.stopAnim();
 });
+incr.addEventListener('click', () => {
+  bpmInput.value++;
+  met.bpm = bpmInput.value;
+});
+decr.addEventListener('click', () => {
+  bpmInput.value--;
+  met.bpm = bpmInput.value;
+});
+
+// Create draggable weight to set BPM on tickhand, lock to Y axis and #tick-hand element
+var weight = Draggable.create('#weight', {
+  type:"y",
+  bounds: document.getElementById("tick-hand"),
+  onDrag: function() {
+    let newBpm = Math.round((met._maxBpm * percentCalc(this.minY, this.maxY, this.y)));
+    console.log(newBpm)
+    bpmInput.value = newBpm;
+    met.bpm = newBpm;
+  }
+});
+// Returns decimal to represent percentage of vertical travel bottom to top of #tick-hand
+function percentCalc(min, max, currentVal) {
+  let temp = (min - currentVal) / min;
+  // console.log(min, max, currentVal, temp);
+  return temp === -0 || temp === 0 ? .01 : temp;
+}
 
 var met = new Metronome('#tick-hand', bpmInput.value, audioClip);
